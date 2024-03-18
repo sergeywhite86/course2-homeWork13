458e57e4c0e7044dd84c7b4884e88e05b0224c96
Sergey White <sergey_white86@mail.ru>
2024-03-18 13:07:44 +0300

    init commit

---
+++ src/main/java/com/white/course2homework13/Course2HomeWork13Application.java
@@ -0,0 +1,13 @@
+package com.white.course2homework13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2HomeWork13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2HomeWork13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/white/course2homework13/Course2HomeWork13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.white.course2homework13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2HomeWork13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
